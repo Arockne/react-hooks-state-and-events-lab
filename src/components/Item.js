1c8e8058785b137b301b68b4e5489aff5cef5078
Daniel Calvin <arockne2003@yahoo.com>
2021-08-27 18:55:30 -0700

    commit tree 8651b39bdc0ef0ad9d4bdafd40e8c64da87155e0

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState('')
+  const buttonText = inCart ? 'Remove From Cart' : 'Add to Cart';
+  const inCartColor = { 
+    background: (inCart ? 'fuchsia' : 'yellow'), 
+    color: (inCart ? 'white' : 'black') 
+  }
+
+
+  function handleClick(event) {
+      const cartState = inCart ? '' : 'in-cart';
+      setInCart(() => cartState)
+  }
+
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick} style={inCartColor}>{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All')
+  const filteredItems = items.filter(({category}) => {
+    if (selectedCategory === 'All') {
+      return true;
+    }
+    return selectedCategory === category;
+  })
+
+  function handleSelection(event) {
+    setSelectedCategory(() => event.target.value)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleSelection}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +34,8 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+//using state
+  //call state variable 'selectedCategory'
+  //when the value of the select element is changed update state
+  //should also use selectedCategory to determine which items to display in the shopping list
